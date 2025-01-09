52bdc29f775633861279e313f6e4ca08f0844dba
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-01-09 22:11:18 +0530

    compltete basic routing and ready basics admin dynamic templates uri's

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import OverviewPage from "./pages/OverviewPage";
 import ProductsPage from "./pages/ProductsPage";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
